5bcf19df9c2d930233141aac2674b1b5d75bd548
C64\COMP64 <movel22@gmail.com>
2019-03-18 00:11:53 +0300

    add node-sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom'
 import './App.css';
+import './nav.scss'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>Тестовое задание ver.2.0</h1> 
+          <h1>Тестовое задание ver.2.0</h1>
+          <nav role="full-horizontal" className="nav">
+            <ul>
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+            </ul>
+          </nav>
           <Route path="/" exact render={() => <h1>Home Page</h1>} />
         </div>
       </div>
