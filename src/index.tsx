5789973fb199a111d5be4f8df1678e7df7ee7be3
C64\COMP64 <movel22@gmail.com>
2019-03-31 23:17:15 +0300

    add redux-devtools

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { RoutedApp } from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+declare global {
+  interface Window { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any; }
+}
+
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 
 const app = (
-  <BrowserRouter>
-    <RoutedApp />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <RoutedApp />
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root') as HTMLElement);
