48b198fc035402d68c3a5dc1bc77aef87fcbf38f
C64\COMP64 <movel22@gmail.com>
2019-04-05 11:10:21 +0300

    add css markup

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
 import { checkAuthStatus, logout } from '../../api/auth';
-import { Button, ButtonToolbar } from 'react-bootstrap'
 
 import './Menu.sass'
+import './Menu.scss'
+
 
 const goTo = (route: string, props: any): any => {
   props.history.replace(`/${route}`)
@@ -13,51 +14,44 @@ const Menu: React.FC<any> = props => {
 
     return (
       <div className="menu">
-        <ButtonToolbar>
-          <Button
+          <button
             className="button__menu"
-            variant="primary"
             onClick={() => goTo('home', props)}
           >
             На главную
-          </Button>
-          <Button
+          </button>
+          <button
             className="button__menu"
-            variant="primary"
             onClick={() => goTo('news', props)}
           >
             Новости
-          </Button>
-          <Button
+          </button>
+          <button
             className="button__menu"
-            variant="primary"
             onClick={() => goTo('profile', props)}
           >
             Профиль
-          </Button>
+          </button>
           {
             !checkAuthStatus() && (
-                <Button
+                <button
                   className="button__menu"
-                  variant="primary"
                   onClick={() => goTo('login', props)}
                 >
                   Log In
-                </Button>
+                </button>
               )
           }
           {
             checkAuthStatus() && (
-                <Button
+                <button
                   className="button__menu"
-                  variant="primary"
                   onClick={() => logout()}
                 >
                   Log Out
-                </Button>
+                </button>
               )
           }
-        </ButtonToolbar>
       </div>
     );
   
